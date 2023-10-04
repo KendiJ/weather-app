8e7611e49b99a42f4e67f16786aad8130b14eafc
Kendi J <kendij@Kendis-MacBook-Pro.local>
2023-10-04 20:17:35 +0300

    first commit

---
+++ lib/presentation/views/weather_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage>{
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
