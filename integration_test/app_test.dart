f0d8f2d6d80899fa6ab5bb2adf841379f72db04a
Kendi J <kendij@Kendis-MacBook-Pro.local>
2023-10-16 21:21:15 +0300

    checks

---
+++ integration_test/app_test.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:integration_test/integration_test.dart';
+// import 'package:weather_app/main.dart' as app;
+// import 'package:weather_app/main.dart';
+
+// void main() {
+
+//   IntegrationTestWidgetsFlutterBinding();
+
+//   group("show current weather and the weather type", () {
+//     testWidgets("correct current weather display", (tester) async {
+//       app.main();
+//       await tester.pumpWidget(const MyApp());
+
+//       await tester.pumpAndSettle(const Duration(seconds: 3));
+
+//       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+//       await tester.pumpAndSettle(const Duration(seconds: 3));
+//     });
+//   });
+// }
+
